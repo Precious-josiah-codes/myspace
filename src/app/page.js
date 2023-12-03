db4ad224afef4102d36ef907497b578864473ee6
Precious-josiah-codes <preciousjosiah0@gmail.com>
2023-12-03 09:00:27 -0800

    new changesh

--- src/app/page.js
+++ src/app/page.js
@@ -26,13 +26,13 @@ export default function Home() {
     }
   };
 
-  useEffect(() => {
-    const handleInitWeb5 = async () => {
-      await initWeb5();
-    };
+  // useEffect(() => {
+  //   const handleInitWeb5 = async () => {
+  //     await initWeb5();
+  //   };
 
-    handleInitWeb5();
-  }, []);
+  //   handleInitWeb5();
+  // }, []);
 
   return (
     <main>
@@ -40,41 +40,6 @@ export default function Home() {
       <p>Building a decentralized cloud storage infastructure</p>
 
       {/* create profile */}
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          createProfile({ name, profileImage, gender });
-        }}
-      >
-        <input
-          type="text"
-          placeholder="enter name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="enter gender"
-          value={gender}
-          onChange={(e) => setGender(e.target.value)}
-        />
-        <input type="file" onChange={handleFileChange} />
-        <button>Create profile</button>
-      </form>
-
-      <button onClick={handleFetch}>Query all data</button>
-      <button onClick={createRecord}>create data</button>
-      <button onClick={createSpace}>create space</button>
-      <button onClick={handleReadSpace}>read space</button>
-      <button onClick={createPublicSpace}>create public space</button>
-      <button onClick={readPublicSpace}>read public space</button>
-
-      <img
-        src={`data:image/jpg;base64,${image}`} // Include the appropriate MIME type
-        alt="Profile Image" // Alt text for accessibility
-        width={300} // Set the width of the image
-        height={300} // Set the height of the image
-      />
     </main>
   );
 
